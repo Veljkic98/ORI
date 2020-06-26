32fef447d42c337bcbbc2184a17373bee417b77e
alekalekalek <alek@spectra-alarm.com>
2020-06-26 10:17:23 +0200

    code refactoring uradjen. Sva pravila su prebacena u posebne funkcije

--- pacman_project2/myTeam.py
+++ pacman_project2/myTeam.py
@@ -24,7 +24,7 @@ from util import nearestPoint
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-               first = 'MojAgent', second = 'MojAgent'):
+               first = 'MojAgent', second = 'DummyAgent'):
   """
   This function should return a list of two agents that will form the
   team, initialized using firstIndex and secondIndex as their agent
@@ -95,10 +95,7 @@ class DummyAgent(CaptureAgent):
 
 
 
-
-
-
-class ReflexCaptureAgent(CaptureAgent):
+class MojAgent(CaptureAgent):
   def registerInitialState(self, gameState):
     self.start = gameState.getAgentPosition(self.index)
     CaptureAgent.registerInitialState(self, gameState)
@@ -123,28 +120,13 @@ class ReflexCaptureAgent(CaptureAgent):
       return bestAction
 
 
-
     values = [self.evaluate(gameState, a) for a in actions]
-
     maxValue = max(values)
     bestActions = [a for a, v in zip(actions, values) if v == maxValue]
 
-
-
-
-
     #ako cu ja pojesti hranu onda stavi flag da sam bas ja pojeo a ne kolega pacman
     choice = random.choice(bestActions)
-    succState = self.getSuccessor(gameState, choice)
-    succAgent = succState.getAgentState(self.index)
-    succFood = len(self.getFood(succState).asList())
-    currFood = len(self.getFood(gameState).asList())
-    if succFood != currFood:
-      print('pojscu hranu')
-      self.jaSamPojeo = True
-    else:
-      self.jaSamPojeo = False
-
+    self.willFoodBeEaten(choice)
     return choice
 
   def getSuccessor(self, gameState, action):
@@ -168,81 +150,9 @@ class ReflexCaptureAgent(CaptureAgent):
     return features * weights
 
 
-class MojAgent(ReflexCaptureAgent):
-  def getFeatures(self, gameState, action):
-
-    features = util.Counter()
-    successor = self.getSuccessor(gameState, action)
-    myState = successor.getAgentState(self.index)
-    myPos = myState.getPosition()
-    foodList = self.getFood(successor).asList()
-    enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-    invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
-
-    if myState.isPacman:
-      features['onDefense'] = 0 #ako je pacman, znaci u napadu je, to se ne kaznjava
-    else:
-      features['onDefense'] = 1 #ako nije pacman vec ghost, znaci u odbrani je, to se kaznjava malo
-
-    features['successorScore'] = -len(foodList) #dobija poene kada pojede bilo sta
-
-    # ako nema napadaca idi da jedes hranu
-    if len(invaders) <= 0:
-      minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
-      features['distanceToFood'] = minDistance
-
-    #ako ima napadaca trci nazad  u odbranu
-    else:
-      dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
-      features['invaderDistance'] = min(dists)
-
-
-    # ako sam u napadu onda bezi od protivnika
-    if myState.isPacman:
-      defenders = [ a for a in enemies if a.isPacman is False and a.getPosition() != None]
-      defendersDists= [self.getMazeDistance(myPos, a.getPosition()) for a in defenders]
-      total = 0
-      for dist in defendersDists:
-        total += dist
-      features['defendersDistance'] = 1/total
-
-    #stajanje u mestu se kaznjava
-    if action == Directions.STOP:
-      features['stop'] = 1
-
-    #vracanje u nazad se isto kaznjava
-    rev = Directions.REVERSE[gameState.getAgentState(self.index).configuration.direction]
-    if action == rev:
-      features['reverse'] = 1
-
-    #da ne budu preblizu oba agenta
-    features['distanceBetweenAgents'] = self.getMazeDistance(
-      successor.getAgentPosition(1),
-      successor.getAgentPosition(3)
-    )
-
-    #ako sam pojeo hranu vrati se nazad
-    #mora try blok jer baca exception u getMazeDistance
-    # kada probam da se pomerim u jednu stranu, a protivnicki pacman me je pojeo
-    try:
-      predecessor = self.getPreviousObservation()
-      if predecessor is not None:
-        pojedenaHrana = len(self.getFood(predecessor).asList()) != len(self.getFood(gameState).asList())
-        if pojedenaHrana is True and self.jaSamPojeo is True:
-          xHome = CaptureAgent.getFood(self,gameState).width // 3 + 1
-          yHome = gameState.getAgentState(self.index).getPosition()[1]
-          homeCoords = (xHome, yHome)
-          features['distanceToHome'] = self.getMazeDistance(myPos, homeCoords)
-    except:
-      pass
-
-
-
-    return features
-
   def getWeights(self, gameState, action):
     return {
-      'successorScore': 100, #ovo je pozitivno, ostalo su penali
+      'score': 100, #ovo je pozitivno, ostalo su penali
       'onDefense': -10, #dajem prednost napadu
       'distanceToFood': -1,
       'invaderDistance': -10,
@@ -252,3 +162,109 @@ class MojAgent(ReflexCaptureAgent):
       'distanceBetweenAgents': 1,
       'defendersDistance': -100,
     }
+
+  def getFeatures(self, gameState, action):
+    #inicijalizuj promenljive koje ce mi trebati
+    self._features = util.Counter()
+    self._succ = self.getSuccessor(gameState, action)
+    self._succState = self._succ.getAgentState(self.index)
+    self._succPos = self._succState.getPosition()
+    self._foodList = self.getFood(self._succ).asList()
+    self._enemies = [self._succ.getAgentState(i) for i in self.getOpponents(self._succ)]
+    self._invaders = [a for a in self._enemies if a.isPacman and a.getPosition() != None]
+    self._gameState = gameState
+    self._action = action
+
+    #pozovi sve funkcije za setovanje feature-a
+    self._init()
+    #vrati feature-a evaluate funkciji
+    return self._features
+
+  def _init(self):
+    self.goAttack()
+    self.setScore()
+    self.playDefenseOrOffense()
+    self.runAwayFromOpponent()
+    self.stop()
+    self.reverse()
+    self.distanceBetweenAgents()
+    self.returnFood()
+
+  ### pravila kojih moj pacman mora da se pridrzava ###
+  #---------------------------------------------------#
+
+  def goAttack(self):
+    if self._succState.isPacman:
+      self._features['onDefense'] = 0 #ako je pacman, znaci u napadu je, to se ne kaznjava
+    else:
+      self._features['onDefense'] = 1 #ako nije pacman vec ghost, znaci u odbrani je, to se kaznjava malo
+
+  def setScore(self):
+    self._features['score'] = -len(self._foodList)  #dobija poene kada pojede bilo sta
+
+  def playDefenseOrOffense(self):
+    # ako nema napadaca idi da jedes najblizu hranu
+    if len(self._invaders) <= 0:
+      minDistance = min([self.getMazeDistance(self._succPos, food) for food in self._foodList])
+      self._features['distanceToFood'] = minDistance
+
+    # ako ima napadaca trci nazad  u odbranu
+    else:
+      dists = [self.getMazeDistance(self._succPos, a.getPosition()) for a in self._invaders]
+      self._features['invaderDistance'] = min(dists)
+
+  def runAwayFromOpponent(self):
+    # ako sam u napadu onda bezi od protivnika
+    if self._succState.isPacman:
+      defenders = [a for a in self._enemies if a.isPacman is False and a.getPosition() != None]
+      defendersDists = [self.getMazeDistance(self._succPos, a.getPosition()) for a in defenders]
+      total = 0
+      for dist in defendersDists:
+        total += dist
+      self._features['defendersDistance'] = 1 / total
+
+  def stop(self):
+    # stajanje u mestu se kaznjava
+    if self._action == Directions.STOP:
+      self._features['stop'] = 1
+
+  def reverse(self):
+    # vracanje u nazad se isto kaznjava
+    rev = Directions.REVERSE[self._gameState.getAgentState(self.index).configuration.direction]
+    if self._action == rev:
+      self._features['reverse'] = 1
+
+  def distanceBetweenAgents(self):
+    # da ne budu preblizu oba agenta
+    self._features['distanceBetweenAgents'] = self.getMazeDistance(
+      self._succ.getAgentPosition(1),
+      self._succ.getAgentPosition(3)
+    )
+
+  def returnFood(self):
+    # ako sam pojeo hranu vrati se nazad
+    # mora try blok jer baca exception u getMazeDistance
+    # kada probam da se pomerim u jednu stranu, a protivnicki pacman me je pojeo
+    try:
+      predecessor = self.getPreviousObservation()
+      if predecessor is not None:
+        pojedenaHrana = len(self.getFood(predecessor).asList()) != len(self.getFood(self._gameState).asList())
+        if pojedenaHrana is True and self.jaSamPojeo is True:
+          xHome = CaptureAgent.getFood(self, self._gameState).width // 3 + 1
+          yHome = self._gameState.getAgentState(self.index).getPosition()[1]
+          homeCoords = (xHome, yHome)
+          self._features['distanceToHome'] = self.getMazeDistance(self._succPos, homeCoords)
+    except:
+      pass
+
+
+  def willFoodBeEaten(self, choice):
+    succState = self.getSuccessor(self._gameState, choice)
+    succAgent = succState.getAgentState(self.index)
+    succFood = len(self.getFood(succState).asList())
+    currFood = len(self.getFood(self._gameState).asList())
+    if succFood != currFood:
+      # print('pojscu hranu')
+      self.jaSamPojeo = True
+    else:
+      self.jaSamPojeo = False
