d3d6f4be549a971c9b807969ef749faf48239a07
alekalekalek <alek@spectra-alarm.com>
2020-07-01 16:01:43 +0200

    promenjen broj filtera

--- klasifikacija_slika_cnn/main.py
+++ klasifikacija_slika_cnn/main.py
@@ -2,13 +2,9 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D,Flatten, Activation, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
-<<<<<<< HEAD
-# from tensorflow import
-=======
 import matplotlib.pyplot as plt
 import warnings
 warnings.simplefilter(action='ignore')
->>>>>>> 6fea7fb86df163aa06bed8b222407cf68a2c0e9d
 
 def main():
     BS = 40
@@ -51,15 +47,15 @@ def main():
 
     model = Sequential()
 
-    model.add(Conv2D(32, (3,3), input_shape=(32,32,3),padding='same'))
+    model.add(Conv2D(64, (3,3), input_shape=(32,32,3),padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3,3),padding='same'))
+    model.add(Conv2D(64, (3,3),padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (3, 3),padding='same'))
+    model.add(Conv2D(128, (3, 3),padding='same'))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
